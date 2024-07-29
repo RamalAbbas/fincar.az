d7026c24b6fd589a299b4835a5ead26273f4b606
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-07-29 16:16:06 +0400

    add

--- src/app/admin/add_product/page.js
+++ src/app/admin/add_product/page.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import Sidebar from "../../../components/Admin/Sidebar/Sidebar";
 import AddProduct from "../../../components/Admin/AddProduct/AddProduct";
