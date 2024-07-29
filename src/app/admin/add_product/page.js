65aa0f95129a276e9bb9db0c982ad58571a3f51c
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-07-29 15:22:07 +0400

    add

--- src/app/admin/add_product/page.js
+++ src/app/admin/add_product/page.js
@@ -1,14 +1,15 @@
+"use client";
 import React from "react";
 import Sidebar from "../../../components/Admin/Sidebar/Sidebar";
 import AddProduct from "../../../components/Admin/AddProduct/AddProduct";
-const page = () => {
+
+const Page = () => {
   return (
     <div style={{ padding: "32px 156px 92px 28px", display: "flex" }}>
       <Sidebar />
-
       <AddProduct />
     </div>
   );
 };
 
-export default page;
+export default Page;
