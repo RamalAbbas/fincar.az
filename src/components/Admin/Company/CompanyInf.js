0a58c1d1784fa65b77234df3206cf22853c482b8
turalma <104642131+turalma@users.noreply.github.com>
2024-08-02 19:54:26 +0400

    tural

--- src/components/Admin/Company/CompanyInf.js
+++ src/components/Admin/Company/CompanyInf.js
@@ -76,6 +76,7 @@ const  DealerUptade = async () => {
     };
   return (
       <>
+    <div className={styles.wraperr}>
     <div className={styles.container}>
     <div className={styles.paragraph}>
     <p className={styles.text}>Company information</p>
@@ -93,13 +94,11 @@ const  DealerUptade = async () => {
            <input type='file' ref={inputRef} className={styles.none} onChange={handleImageChange} placeholder='Replace'></input>
            </button> 
     </div> 
+    <div className={styles.main}>
+      <div className={styles.parent}>
     <div className={styles.inp}>
           <label className={styles.username}>Company name</label>
           <input type='text' placeholder='AutoStar Kaukasus GmbH' className={styles.wraper} name='name' value={data.name} onChange={handleInputChange}/>
-    </div>
-    <div className={styles.inp3}>
-          <label className={styles.username}>Working hour</label>
-          <input type='text' placeholder='09:00  19:00' className={styles.wraper} name='opening_time' value={data.opening_time} onChange={handleInputChange}/>
     </div>
     <div className={styles.inp4}>
           <label className={styles.username}>City</label>
@@ -108,6 +107,14 @@ const  DealerUptade = async () => {
             </select>
           {/* <input type='text' placeholder='Bakı' className={styles.wraper} name='city' value={data.city} onChange={handleInputChange} /> */}
     </div> 
+    <div className={styles.inp3}>
+          <label className={styles.username}>Working hour</label>
+          <input type='text' placeholder='09:00  19:00' className={styles.wraper} name='opening_time' value={data.opening_time} onChange={handleInputChange}/>
+    </div>
+    <div className={styles.inp6}>
+          <label className={styles.username}>Street</label>
+           <input type='text' placeholder='Zaur Nudirəliyev 89a' className={styles.wraper} name='street' value={data.street} onChange={handleInputChange}/> 
+    </div>
     {inputFields.map((field, index) => (
     <div className={styles.inp5}>
           <label className={styles.username}>Phone number</label>
@@ -116,17 +123,17 @@ const  DealerUptade = async () => {
           <input type='text' placeholder='+994 55 654 76 78' className={styles.wraper} name='phone3' value={data.phone3} onChange={handleInputChange}/>
     </div>
     ))}
-    <div className={styles.inp6}>
-          <label className={styles.username}>Street</label>
-           <input type='text' placeholder='Zaur Nudirəliyev 89a' className={styles.wraper} name='street' value={data.street} onChange={handleInputChange}/> 
-    </div>
+
     <div className={styles.inp7}>
           <label className={styles.username}>About your company</label>
-          <input type='text' placeholder='"AutoStar Kaukasus GmbH" MMC - Fiat avtomobillərinin Azərbaycanda rəsmi düstribüteri. 20% ilkin ödəniş,10% illik bank faizi, 5 illik kredit, yürüşdən asılı olmayaraq 2 il zəmanət.' className={styles.wraper1} name='about' value={data.about} onChange={handleInputChange}/>
+          <input type='text' placeholder='AutoStar Kaukasus GmbH" MMC - Fiat avtomobillərinin Azərbaycanda rəsmi düstribüteri. 20% ilkin ödəniş,10% illik bank faizi, 5 illik kredit, yürüşdən asılı olmayaraq 2 il zəmanət.' className={styles.wraper1} name='about' value={data.about} onChange={handleInputChange}/>
     </div>
     <div className={styles.save}>
           <button className={styles.btn1}>Save</button>
         </div>
+        </div>
+    </div>
+    </div>
     </div>
     <div className={styles.responsive}>
 
