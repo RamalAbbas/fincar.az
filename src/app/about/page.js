3eff40cf1dd0a50741b54c4770d4fb2e6da23262
turalma <104642131+turalma@users.noreply.github.com>
2024-08-20 13:53:44 +0400

    tural

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,10 @@ import styles from "./About.module.css";
 import Image from "next/image";
 import headerBg from "../../assets/images/headerBg.png";
 import azFlag from "../../assets/icons/azFlag.svg";
+import about from "../../assets/icons/about/OheU7K.tif.svg"
+import Frame from "../../assets/images/about/Frame 247.png"
+import good from "../../assets/icons/about/good.svg"
+import call from "../../assets/icons/about/call.svg"
 import mobileMenu from "../../assets/icons/mobileHeader/menu.svg";
 import { useRouter } from 'next/router';
 const page = () => {
@@ -45,12 +49,32 @@ const page = () => {
       <h1 className={styles.desc}>Vaxtınıza qənaət edin, vasitəçisiz ən yaxşı təklif alın</h1>
       </div>
       </div>
-
+      <div className={styles.container}>
       <div className={styles.main}>
         <p className={styles.text}>Haqqımızda</p>
       </div>
       <div className={styles.contents}>
         <p className={styles.texts}>Fincar avtomobil alqı-satqısı və icarəsi sahəsində inqilabi bir addımdır. Biz, müştərilərimizin avtomobil sahəsində bütün ehtiyaclarını qarşılamaq üçün buradayıq. Fincar komandası, geniş seçim imkanı və etibarlı xidmətlərlə sizin avtomobil təcrübənizi ən yüksək səviyyəyə çatdırmağa sadiqdir.</p>
+      </div>
+      <div className={styles.iconcontainer}>
+      <div className={styles.icon}>
+      <Image src={about}></Image>
+      <p className={styles.icontxt}>100% Məmnuniyyət</p>
+      </div>
+      <div className={styles.icon}>
+      <Image src={good}></Image>
+      <p className={styles.icontxt}>Maliyyələşdirmə Seçimləri</p>
+      </div>
+      <div className={styles.icon}>
+      <Image src={call}></Image>
+      <p className={styles.icontxt}>Etibarlılıq</p>
+      </div>
+      </div>
+      </div>
+      <div className={styles.fincar}>
+        <div className={styles.fincarr}>
+        <Image src={Frame}></Image>
+        </div>
       </div>
      </>
   )
