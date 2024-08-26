c47449444e989aef4ab534d568551707da6704e5
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-08-26 11:39:57 +0400

    add

--- src/app/admin/edit_product/[slug]/page.js
+++ src/app/admin/edit_product/[slug]/page.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Sidebar from "../../../../components/Admin/Sidebar/Sidebar";
 import EditProduct from "../../../../components/Admin/EditProduct/EditProduct";
-
+import styles from './style.module.css'
 const Page = ({params}) => {
   
   return (
-    <div style={{ padding: "32px 156px 92px 28px", display: "flex" }}>
-      <Sidebar />
-      <EditProduct slug={params.slug} />
+    <div className={`${styles.wrapper}`} style={{display: "flex",justifyContent: 'center', padding: "32px 0px 92px 0px"}} >
+      <div className={styles.container}>
+        <Sidebar />
+        <EditProduct slug={params.slug} />
+      </div>
     </div>
   );
 };
